74e74eeda4a794d95dbbe722eed104c133f6790a
Nathan Guedes Pessoa <90534425+NathanGuedes@users.noreply.github.com>
2026-01-28 23:03:14 -0300

    Update Main to fetch years and find the latest year
    - Update `BASE_URI` to target the "demonstracoes_contabeis" directory.
    - Replace raw HTML request with `client.getYears` to retrieve available years.
    - Add `getBiggestYear` method to identify the maximum year from the list.

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -4,14 +4,28 @@ import com.client.ApiClient;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
-        String BASE_URI = "https://dadosabertos.ans.gov.br/FTP/PDA/";
+        String BASE_URI = "https://dadosabertos.ans.gov.br/FTP/PDA/demonstracoes_contabeis/";
 
         ApiClient client = new ApiClient();
-        String html = client.requestHtml(BASE_URI);
+        List<String> years = client.getYears(BASE_URI);
 
-        System.out.println(html);
+        int biggestYear = getBiggestYear(years);
+
+    }
+
+    private static int getBiggestYear(List<String> years) {
+        int biggestYear = Integer.parseInt(years.get(0));
+
+        for (String year : years) {
+            int value = Integer.parseInt(year);
+            if (value > biggestYear){
+                biggestYear = value;
+            }
+        }
+        return biggestYear;
     }
 }
