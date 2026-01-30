4538c035a70baf3ebed794f8cf5063e88301ed51
Nathan Guedes Pessoa <90534425+NathanGuedes@users.noreply.github.com>
2026-01-29 23:23:08 -0300

    Integrate downloading and filtering workflow in Main
    - Change report limit from 4 to 3 in `findRecentReportUrls`.
    - Instantiate `ArchiveHandler` to download and extract the retrieved links.
    - List files from the "extract" directory.
    - Invoke `FileIOService` to filter the processed files for "Despesas com Eventos/Sinistros".

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,18 +1,38 @@
 package com;
 
+import com.support.ArchiveHandler;
+import com.support.FileIOService;
 import com.support.QuarterlyReportScraper;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         String BASE_URL = "https://dadosabertos.ans.gov.br/FTP/PDA/demonstracoes_contabeis/";
 
         QuarterlyReportScraper client = new QuarterlyReportScraper();
 
-        List<String> files = client.findRecentReportUrls(BASE_URL, 4);
+        List<String> links = client.findRecentReportUrls(BASE_URL, 3);
 
-        System.out.println(files);
+        ArchiveHandler fileDownloader = new ArchiveHandler(BASE_URL, links);
+        fileDownloader.ArchiveDownloader();
+
+        FileIOService fileIOService = new FileIOService();
+
+        Path dir = Path.of(System.getProperty("user.dir"), "extract");
+
+        List<Path> files;
+        try (Stream<Path> paths = Files.list(dir)) {
+            files = paths
+                    .filter(Files::isRegularFile)
+                    .toList();
+        }
+
+        fileIOService.filterFile(files,"DESCRICAO" ,"Despesas com Eventos/Sinistros");
     }
 }
