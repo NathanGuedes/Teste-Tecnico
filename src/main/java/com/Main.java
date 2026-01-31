88e0d0d915226afd3c79e1d279052542592a882a
Nathan Guedes Pessoa <90534425+NathanGuedes@users.noreply.github.com>
2026-01-30 21:04:00 -0300

    Update Main.java
    Refactor Main to implement filtering and concatenation workflow
    
    - Optimize imports by replacing wildcard with `List`.
    - Rename `dir` to `extractDir` and `files` to `extractedFiles` for clarity.
    - Update `filterFile` call with the correct separator `";"`.
    - Retrieve filtered files from the archives directory.
    - Invoke `concatCsvFiles` to merge the processed CSV files.

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -7,7 +7,7 @@ import com.support.QuarterlyReportScraper;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Main {
@@ -16,7 +16,6 @@ public class Main {
         String BASE_URL = "https://dadosabertos.ans.gov.br/FTP/PDA/demonstracoes_contabeis/";
 
         QuarterlyReportScraper client = new QuarterlyReportScraper();
-
         List<String> links = client.findRecentReportUrls(BASE_URL, 3);
 
         ArchiveHandler fileDownloader = new ArchiveHandler(BASE_URL, links);
@@ -24,15 +23,31 @@ public class Main {
 
         FileIOService fileIOService = new FileIOService();
 
-        Path dir = Path.of(System.getProperty("user.dir"), "extract");
+        Path extractDir = Path.of(System.getProperty("user.dir"), "extract");
 
-        List<Path> files;
-        try (Stream<Path> paths = Files.list(dir)) {
-            files = paths
+        List<Path> extractedFiles;
+        try (Stream<Path> paths = Files.list(extractDir)) {
+            extractedFiles = paths
                     .filter(Files::isRegularFile)
                     .toList();
         }
 
-        fileIOService.filterFile(files,"DESCRICAO" ,"Despesas com Eventos/Sinistros", "");
+        fileIOService.filterFile(
+                extractedFiles,
+                "DESCRICAO",
+                "Despesas com Eventos/Sinistros",
+                ";"
+        );
+
+        Path archivesDir = fileIOService.getArchivesDir();
+
+        List<Path> filesToConcat;
+        try (Stream<Path> paths = Files.list(archivesDir)) {
+            filesToConcat = paths
+                    .filter(Files::isRegularFile)
+                    .toList();
+        }
+
+        fileIOService.concatCsvFiles(filesToConcat);
     }
 }
