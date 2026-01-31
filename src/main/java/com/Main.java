755d62ef49df3ba34de5895fd65094764a805ad2
Nathan Guedes Pessoa <90534425+NathanGuedes@users.noreply.github.com>
2026-01-30 21:39:53 -0300

    Update Main.java
    Update Main.java to remove duplicates from consolidated file
    
    - Retrieve the concatenation directory and resolve the consolidated file path.
    - Invoke `fileIOService.removeDuplicates` to process the consolidated CSV.
    - Reformat code indentation and style.

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -3,7 +3,6 @@ package com;
 import com.support.ArchiveHandler;
 import com.support.FileIOService;
 import com.support.QuarterlyReportScraper;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,43 +10,40 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
 
-        String BASE_URL = "https://dadosabertos.ans.gov.br/FTP/PDA/demonstracoes_contabeis/";
+    String BASE_URL = "https://dadosabertos.ans.gov.br/FTP/PDA/demonstracoes_contabeis/";
 
-        QuarterlyReportScraper client = new QuarterlyReportScraper();
-        List<String> links = client.findRecentReportUrls(BASE_URL, 3);
+    QuarterlyReportScraper client = new QuarterlyReportScraper();
+    List<String> links = client.findRecentReportUrls(BASE_URL, 3);
 
-        ArchiveHandler fileDownloader = new ArchiveHandler(BASE_URL, links);
-        fileDownloader.ArchiveDownloader();
+    ArchiveHandler fileDownloader = new ArchiveHandler(BASE_URL, links);
+    fileDownloader.ArchiveDownloader();
 
-        FileIOService fileIOService = new FileIOService();
+    FileIOService fileIOService = new FileIOService();
 
-        Path extractDir = Path.of(System.getProperty("user.dir"), "extract");
+    Path extractDir = Path.of(System.getProperty("user.dir"), "extract");
 
-        List<Path> extractedFiles;
-        try (Stream<Path> paths = Files.list(extractDir)) {
-            extractedFiles = paths
-                    .filter(Files::isRegularFile)
-                    .toList();
-        }
-
-        fileIOService.filterFile(
-                extractedFiles,
-                "DESCRICAO",
-                "Despesas com Eventos/Sinistros",
-                ";"
-        );
-
-        Path archivesDir = fileIOService.getArchivesDir();
-
-        List<Path> filesToConcat;
-        try (Stream<Path> paths = Files.list(archivesDir)) {
-            filesToConcat = paths
-                    .filter(Files::isRegularFile)
-                    .toList();
-        }
-
-        fileIOService.concatCsvFiles(filesToConcat);
+    List<Path> extractedFiles;
+    try (Stream<Path> paths = Files.list(extractDir)) {
+      extractedFiles = paths.filter(Files::isRegularFile).toList();
     }
+
+    fileIOService.filterFile(extractedFiles, "DESCRICAO", "Despesas com Eventos/Sinistros", ";");
+
+    Path archivesDir = fileIOService.getArchivesDir();
+
+    List<Path> filesToConcat;
+    try (Stream<Path> paths = Files.list(archivesDir)) {
+      filesToConcat = paths.filter(Files::isRegularFile).toList();
+    }
+
+    fileIOService.concatCsvFiles(filesToConcat);
+
+    Path archivesConcatDir = fileIOService.getConcatArchiveDir();
+
+    Path concatFile = archivesConcatDir.resolve("consolidated_quarters_by_description.csv");
+
+    fileIOService.removeDuplicates(concatFile, true);
+  }
 }
